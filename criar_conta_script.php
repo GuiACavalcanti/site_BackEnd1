70febf563d09f97af5ffb7a90cab66187cee6085
GuiACavalcanti <139710467+GuiACavalcanti@users.noreply.github.com>
2023-10-18 18:06:31 -0300

    add file

--- criar_conta_script.php
+++ criar_conta_script.php
@@ -3,22 +3,25 @@ include("Conect_mysql.php");
 $nome_txt = $_POST['nome'];
 $email_txt = $_POST['email'];
 $senha_txt = $_POST['senha'];
-$sql = mysqli_query($con, "SELECT distinct * FROM site1.site_1 where email = '$email_txt';");
+$sql = mysqli_query($con, "SELECT * FROM site1.site_1 where email = '$email_txt';");
 $User_login_email = mysqli_fetch_assoc($sql);
-if ($nome_txt == null || $email_txt == null || $email_txt == null) {
+
+if ($User_login_email['email'] == $email_txt && $nome_txt == $$User_login_email['nome']) {
+    echo "
+    <script>
+        alert('Erro: Usuario já existe!');
+        location.href = 'criar_conta.html';
+    </script>
+    ";
+} else if ($nome_txt == null || $email_txt == null || $email_txt == null) {
     echo "<script>alert('Erro: O formulario deve ser preenchido'); location.href = 'criar_conta.html';</script>";
-} else if ($email_txt == $User_login_email['email'] && $nome_txt == $User_login_email['nome']) {
-    echo "<script>
-    alert('Erro: Usuario já existe!');
-    location.href = 'criar_conta.html';
-    </script>";
-} else if ($con->query("INSERT INTO site_1 (nome, email, senha) VALUE ('$nome_txt', '$email_txt', '$senha_txt');") === true) {
-        echo "
-        <script>
-            alert('Usuario cadastrado com sucesso!')
-            location.href = 'criar_conta.html';
-        </script>
-        ";
+} else if ($con = "INSERT INTO site_1 (nome, email, senha) VALUE ('$nome_txt', '$email_txt', '$senha_txt');" === true) {
+    echo "
+    <script>
+        alert('Usuario cadastrado com sucesso!')
+        location.href = 'criar_conta.html';
+    </script>
+    ";
 } else {
     echo "
     <script>
--- index.php
+++ index.php
@@ -4,29 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Usuários</title>
-    <style>
-        body {
-            font-family: Arial, Helvetica, sans-serif;
-            background: #666769;
-        }
-        #btn1 {
-            display: block;
-            position: absolute;
-        }
-        #btn2 {
-            display: block;
-            position: absolute;
-            left: 65px;
-        }
-        #btn3 {
-            display: block;
-        }
-    </style>
+    <link rel="stylesheet" href="style_index.css">
 </head>
 <body>
-    <?php session_start(); ?>
     <div>
-        <h3 id="txt1">Bem vindo <?php echo $_SESSION['nome_user'] ?>!</h3>
+        <h1><?php  ?></h1>
+        <h3 id="txt1">Bem vindo, <?php echo $_SESSION['nome_user'] ?>!</h3>
         <button id="btn1" onclick="location.href = 'login_entrar.html'">Entrar</button>
         <button id="btn2" onclick="location.href = 'criar_conta.html'">Criar conta</button>
         <form action="deixar_null.php" method="post">
@@ -39,10 +22,13 @@
             document.getElementById('btn1').style.display = 'block';
             document.getElementById('btn2').style.display = 'block';
             document.getElementById('btn3').style.display = 'none';
-        }
     </script>
 </body>
 </html>
+<?php session_start(); ?>
+
+// Se não tiver nada no nome_user ele vai esconder o Bem vindo, nome_user! e esconder os botoes
+// E sé tiver algo ele vai esconder os botao é mostrar o bem vindo
 <?php 
 if ($_SESSION['nome_user'] != null) {
     echo "<script>document.getElementById('btn1').style.display = 'none';</script>";
